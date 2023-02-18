d60ea1cbf34fd3718dc0c00d93d2d4e009ca6ba1
cauemondek <121320616+GuiseM@users.noreply.github.com>
2023-02-17 23:17:02 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,103 +1,93 @@
 
-// let editOnList = document.querySelectorAll('.icon-edit')
-
-const addList = document.getElementById('addInList')
-let itemInput = document.getElementById('itemInput')
+const addList = document.getElementById('addInList');
+let itemInput = document.getElementById('itemInput');
 
 const waitSecure = document.getElementById('wait');
 
-const list = document.getElementById('list')
+const list = document.getElementById('list');
 
 document.addEventListener('keyup',(e)=>{
-    let type = e.keyCode
+    let type = e.keyCode;
     
     if (itemInput.value == ""){
-        alert("You have to type something first")
+        alert("You have to type something first");
     } else {
         if (type === 13) {
-        createItem()
-        }
-    }
-})
-
+        createItem();
+        };
+    };
+});
 
 addList.addEventListener('click', ()=>{
     if (itemInput.value == ""){
-        alert("You have to type something first")
+        alert("You have to type something first");
     } else {
-        createItem()
-    }
-})
-
-
+        createItem();
+    };
+});
 
 let checkOnList;
 let lineItem;
 
 function createItem(){
-
-    const line = document.createElement("div")
-    const item = document.createElement("li")
-    const ok = document.createElement("i")
-    // const edit = document.createElement("i")
+    const line = document.createElement("div");
+    const item = document.createElement("li");
+    const ok = document.createElement("i");
     
-    line.setAttribute("class", "line")
-    item.setAttribute("class", "text")
+    line.setAttribute("class", "line");
+    item.setAttribute("class", "text");
     
     ok.innerHTML = "<i class='icon-check fa-solid fa-check'></i>";
-    // edit.innerHTML = "<i class='icon-edit fa-solid fa-pen'></i>";
     
     item.innerHTML = itemInput.value;
     itemInput.value = "";
     
     list.appendChild(line);
-    line.appendChild(item)
-    line.appendChild(ok)
-    // line.appendChild(edit)
+    line.appendChild(item);
+    line.appendChild(ok);
     
-    checkOnList = document.querySelectorAll('.icon-check')
-    lineItem = document.querySelectorAll('.line')
-    textItem = document.querySelectorAll('.text')
-    underlineItem = document.querySelectorAll('.line li')
-    
+    checkOnList = document.querySelectorAll('.icon-check');
+    lineItem = document.querySelectorAll('.line');
+    textItem = document.querySelectorAll('.text');
+    underlineItem = document.querySelectorAll('.line li');
     
     checkOnList.forEach((index, i)=>{
         index.addEventListener('click',()=>{
-            secureLoad()
-            console.log(i)
-            index.style.color = "#0dc250"
-            index.style.scale = "1.2"
+            secureLoad();
+            console.log(i);
+            index.style.color = "#0dc250";
+            index.style.scale = "1.2";
             
-            underlineItem[i].style.textDecoration = 'none'
+            underlineItem[i].style.textDecoration = 'none';
             
-            textItem[i].removeClass
-            lineItem[i].style.textDecoration = 'line-through #000'
-            textItem[i].style.color = '#0dc250'
-            lineItem[i].style.transition = '2.5s'
+            textItem[i].removeClass;
+            lineItem[i].style.textDecoration = 'line-through #000';
+            textItem[i].style.color = '#0dc250';
+            lineItem[i].style.transition = '2.5s';
             
             setTimeout(()=>{
-                lineItem[i].style.opacity = '0'
-            }, 500)
+                lineItem[i].style.opacity = '0';
+            }, 500);
             setTimeout(()=>{
-                list.removeChild(lineItem[i])
-            }, 2600)
-        })
-    })
-}
+                list.removeChild(lineItem[i]);
+            }, 2600);
+        });
+    });
+};
 
 function secureLoad(){
-    addList.style.display = 'none'
-    itemInput.style.display = 'none'
-    animation()
+    addList.style.display = 'none';
+    itemInput.style.display = 'none';
+    animation();
     setTimeout(()=>{
-        addList.style.display = 'block'
-        itemInput.style.display = 'block'
-        waitSecure.style.display = 'none'
-    }, 3000)
-}
+        addList.style.display = 'block';
+        itemInput.style.display = 'block';
+        waitSecure.style.display = 'none';
+    }, 3000);
+};
 
 function animation(){
-    waitSecure.style.display = 'block'
+    waitSecure.style.display = 'block';
     waitSecure.innerHTML = "";
 setTimeout(function(){
     waitSecure.innerHTML = "Wait...";
