dee11ef75aeff9ac19134d4a160ea1099df166eb
cauemondek <121320616+GuiseM@users.noreply.github.com>
2023-02-17 23:12:04 -0300

    I add my main files via upload

---
+++ js/app.js
@@ -0,0 +1,129 @@
+
+// let editOnList = document.querySelectorAll('.icon-edit')
+
+const addList = document.getElementById('addInList')
+let itemInput = document.getElementById('itemInput')
+
+const waitSecure = document.getElementById('wait');
+
+const list = document.getElementById('list')
+
+document.addEventListener('keyup',(e)=>{
+    let type = e.keyCode
+    
+    if (itemInput.value == ""){
+        alert("You have to type something first")
+    } else {
+        if (type === 13) {
+        createItem()
+        }
+    }
+})
+
+
+addList.addEventListener('click', ()=>{
+    if (itemInput.value == ""){
+        alert("You have to type something first")
+    } else {
+        createItem()
+    }
+})
+
+
+
+let checkOnList;
+let lineItem;
+
+function createItem(){
+
+    const line = document.createElement("div")
+    const item = document.createElement("li")
+    const ok = document.createElement("i")
+    // const edit = document.createElement("i")
+    
+    line.setAttribute("class", "line")
+    item.setAttribute("class", "text")
+    
+    ok.innerHTML = "<i class='icon-check fa-solid fa-check'></i>";
+    // edit.innerHTML = "<i class='icon-edit fa-solid fa-pen'></i>";
+    
+    item.innerHTML = itemInput.value;
+    itemInput.value = "";
+    
+    list.appendChild(line);
+    line.appendChild(item)
+    line.appendChild(ok)
+    // line.appendChild(edit)
+    
+    checkOnList = document.querySelectorAll('.icon-check')
+    lineItem = document.querySelectorAll('.line')
+    textItem = document.querySelectorAll('.text')
+    underlineItem = document.querySelectorAll('.line li')
+    
+    
+    checkOnList.forEach((index, i)=>{
+        index.addEventListener('click',()=>{
+            secureLoad()
+            console.log(i)
+            index.style.color = "#0dc250"
+            index.style.scale = "1.2"
+            
+            underlineItem[i].style.textDecoration = 'none'
+            
+            textItem[i].removeClass
+            lineItem[i].style.textDecoration = 'line-through #000'
+            textItem[i].style.color = '#0dc250'
+            lineItem[i].style.transition = '2.5s'
+            
+            setTimeout(()=>{
+                lineItem[i].style.opacity = '0'
+            }, 500)
+            setTimeout(()=>{
+                list.removeChild(lineItem[i])
+            }, 2600)
+        })
+    })
+}
+
+function secureLoad(){
+    addList.style.display = 'none'
+    itemInput.style.display = 'none'
+    animation()
+    setTimeout(()=>{
+        addList.style.display = 'block'
+        itemInput.style.display = 'block'
+        waitSecure.style.display = 'none'
+    }, 3000)
+}
+
+function animation(){
+    waitSecure.style.display = 'block'
+    waitSecure.innerHTML = "";
+setTimeout(function(){
+    waitSecure.innerHTML = "Wait...";
+}, 300);
+setTimeout(function(){
+    waitSecure.innerHTML = "";
+}, 600);
+setTimeout(function(){
+    waitSecure.innerHTML = "Wait...";
+}, 900);
+setTimeout(function(){
+    waitSecure.innerHTML = "";
+}, 1200);
+setTimeout(function(){
+    waitSecure.innerHTML = "Wait...";
+}, 1500);
+setTimeout(function(){
+    waitSecure.innerHTML = "";
+}, 1800);
+setTimeout(function(){
+    waitSecure.innerHTML = "Wait...";
+}, 2100);
+setTimeout(function(){
+    waitSecure.innerHTML = "";
+}, 2400);
+setTimeout(function(){
+    waitSecure.innerHTML = "Wait...";
+}, 2700);
+};
